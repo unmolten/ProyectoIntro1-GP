03862f3cf618d4b5928f749f61f0273533acde9b
gabrielp <gabpj1209@gmail.com>
2026-04-28 12:10:27 -0600

    Ajustar dimensiones del canvas y modificar la lógica de creación de obstáculos

--- juego.py
+++ juego.py
@@ -9,8 +9,8 @@ root.geometry("1000x750+100+100") # Dimensiones de ventana, el +100+100 coloca l
 root.resizable(0,0) # Evita cambiar la ventana de tamaño
 
 # Variables para tener tamaños constantes
-canvx = 900 # grosor
-canvy = 750 # altura
+canvx = 500 # grosor
+canvy = 500 # altura
 # Creacion de un canvas para colocar los objetos dentro de el:
 canvJuego = tk.Canvas(root,bg="#555555",width=canvx,height=canvy) # Se asignan los valores para las propiedades
 canvJuego.pack() # Se coloca el canvas
@@ -21,13 +21,20 @@ canvJuego.pack() # Se coloca el canvas
 # [2] = x1, 
 # [3] = y1
 p1 = canvJuego.create_rectangle(0,canvy,30,canvy-50,fill="red") # Se crea un rectangulo con las coordenadas dadas y de color rojo
-p1pies = canvJuego.create_rectangle(0,canvy,30,canvy-10,fill="green") 
-caja = canvJuego.create_rectangle(50, canvy - 150, 450, canvy- 100,fill="blue")
+
+mapa = [
+    [0,0,0,0,0],
+    [1,1,1,0,0],
+    [0,0,0,0,0],
+    [1,0,0,1,0],
+    [0,0,0,0,1]
+]
+
+cajasmapa = []
 
 
 bucle = None # Guarda el ID del after() para luego poder cancelar el bucle
 saltando = False # Guarda el estado de salto
-aire = False
 vely = 0 # velocidad en el eje y (no cambia la altura del salto desde acá)
 velx = 10 # velocidad en el eje x (no se cambia mucho)
 
@@ -74,11 +81,13 @@ def mover():
         # Revisa si se mantiene el boton, y se esta dentro del marco del canvas
         if d_held and x1 <= canvx:
             canvJuego.move(p1, velx, 0) # Al personaje (p1) se mueve a la derecha y 0 hacia arriba
-            canvJuego.move(p1pies, velx, 0)
         elif a_held and x0 >= 0:
-            canvJuego.move(p1, -velx, 0)
-            canvJuego.move(p1pies, -velx, 0) # Al personaje (p1) se mueve a la izq y 0 hacia arriba
+            canvJuego.move(p1, -velx, 0) # Al personaje (p1) se mueve a la izq y 0 hacia arribaa
         ultimo_frame = ahora
+    y1 = canvJuego.coords(p1)[3]
+
+    pass
+            
 
     # Se hace el bucle para que sea consistente
     if d_held or a_held:
@@ -94,8 +103,6 @@ def saltar(evento):
     # Revisa si no esta en el aire
     if not saltando:
         vel_y=0
-        canvJuego.move(p1, 0, -5) 
-        canvJuego.move(p1pies,0,-5)
         saltando = True 
         aire = True
         vel_y = -20 # esta variable modifica la altura del salto
@@ -107,22 +114,14 @@ def saltar(evento):
 # Se tiene que reescribir el sistema de gravedad porque hace demasiadas llamadas recursivas innecesarias con lo de colision.
 def gravedad():
     global saltando, vel_y, aire
-    if aire and colision() == "nada":
-        canvJuego.move(p1, 0, vel_y)  # Va moviendo el jugador verticalmente, empieza llendo para arriba, y va desacelerando 
-        canvJuego.move(p1pies,0,vel_y)
-        vel_y += 1 # Aumenta la velocidad para abajo
+    canvJuego.move(p1, 0, vel_y)  # Va moviendo el jugador verticalmente, empieza llendo para arriba, y va desacelerando 
+    vel_y += 1 # Aumenta la velocidad para abajo
 
     y1 = canvJuego.coords(p1)[3] # Coordenada de parte de abajo del personaje
-
-    if colision() == "pies":
-        vel_y = 0
-        saltando = False
-           
 
     # revisa si choca con el piso, y para de mover hacia abajo
     if y1 >= canvy - 10:
         canvJuego.move(p1, 0, canvy - y1) # mueve el personaje al nivel piso para evitar que se quede pegado el jugador
-        canvJuego.move(p1pies, 0, canvy - y1)
         saltando = False
         aire = False
         vel_y = 0 # quita la velocidad
@@ -130,18 +129,24 @@ def gravedad():
         root.after(18, gravedad) # bucle para el movimiento
 
 
-def colision():
-    x0 = canvJuego.coords(caja)[0]
-    y0 =canvJuego.coords(caja)[1]
-    x1 =canvJuego.coords(caja)[2]
-    y1 = canvJuego.coords(caja)[3]
-    colisionx = canvJuego.find_overlapping(x0,y0,x1,y1)
-    if colisionx == (1,3):
-        return "nopies"
-    elif colisionx == (1,2,3):
-        return "pies"
-    elif colisionx == (3,):
-        return "nada"
+def crear_cajas():
+    global cajasmapa
+    y0 = 0
+    x0 = 0
+    for i in range(len(mapa)):
+        fila = mapa[i]
+        print(y0)
+        for j in range(len(mapa[i])):
+            if fila[j] == 1:
+                cajasmapa += [canvJuego.create_rectangle(x0,y0,x0 + 100, y0 + 100,fill="green")]
+                print(x0)
+            x0 += 100
+        y0 += 100
+        x0 = 0
+
+
+crear_cajas()
+print(cajasmapa)
 
 """ Funciones un poco complicadas, lo que hacen es que detectan cada ingreso de la tecla y .bind() les asigna un evento.
 para poder llamar otras funciones con otras variables, se hace un lambda con parametro evento para que pueda guardar lo que asigna
